e4a2edd037ac468ef0e55fc9e28e8c5eb35effbd
Nat345 <natalie.hale1@education.nsw.gov.au>
2021-10-19 11:14:40 +1100

    19/10 11:14

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,7 @@
-print("hi")
+x = int(input("type in a number" ))
+y =int(input("type in another number" ))
+print(x+y)
+
+x = int(input("type in a number" ))
+y =int(input("type in another number" ))
+print(x+y)

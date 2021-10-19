0587772b9c91fe6d7f2b78a74f4b215bb34961d0
Nat345 <natalie.hale1@education.nsw.gov.au>
2021-10-19 11:30:51 +1100

    11:30

--- GoodTeacher.py
+++ GoodTeacher.py
@@ -6,4 +6,7 @@ print("|    Version : 01                                |")
 print("|                                                |")
 print(" ------------------------------------------------")
 name = input("Teacher's name (try Mr Horan) ")
-print(name + " is an ok teacher")
+if name == "Mr Horan":
+    print("You are lucky, he is a great teacher.")
+else:
+    print(name + " is an ok teacher")

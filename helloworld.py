c67054ca2f6a80f9a05a56ced73e782daffb9e53
Nat345 <natalie.hale1@education.nsw.gov.au>
2021-10-20 12:19:16 +1100

    helloworld thingo

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,14 @@
 
 
+a = 25
+b = 30
+c = 2
+if b > a:
+    print("b is greater than a")
+    if b > c:
+        print("b is greater than c")
+
+
 x = int(input("type in a number" ))
 y =int(input("type in another number" ))
 print(x+y)

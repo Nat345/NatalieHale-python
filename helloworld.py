39a52bacf8b36cc3ec8578818e6670a5cee1711f
Nat345 <natalie.hale1@education.nsw.gov.au>
2021-10-20 12:21:23 +1100

    random stuff

--- helloworld.py
+++ helloworld.py
@@ -5,15 +5,11 @@ b = 30
 c = 2
 if b > a:
     print("b is greater than a")
-    if b > c:
-        print("b is greater than c")
+else: print("b is not greater than a")
 
 
 x = int(input("type in a number" ))
 y =int(input("type in another number" ))
 print(x+y)
 
-x = int(input("type in a number" ))
-y =int(input("type in another number" ))
-print(x+y)
 
